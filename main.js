b2da1a670294f0d0d1e205095cf7753ca2833aae
Todd Wolfe <wolfet410@yahoo.com>
2013-05-28 22:20:49 -0400

    v0.2.0 - Started schedule page

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
 });
 
 function showSchedule(season) {
+  current = '9';
+
+  // I want buildScheduleTable to tell me what the highest (current) season is too, 
+  // for now I'm hard coding it
+  // I also want the page to default to the current season, which I can handle by
+  // running showSchedule on pageshow with no parameters, and saying that season = null then season = current
+  // This doesn't work for shit yet, but moving on
+  $('.sbtn').each(function () {
+    $(this).attr('data-theme', 'a').removeClass('ui-btn-up-c').addClass('ui-btn-up-a');
+  });
+  $('#s'+season).attr('data-theme', 'c').removeClass('ui-btn-up-a').addClass('ui-btn-up-c');
+
   $.ajax({
     type: 'POST',
     url: 'main.php',
@@ -12,7 +24,45 @@ function showSchedule(season) {
       todo: "buildScheduleTable",
       season: season
     },
-    cache: false
+    dataType: 'json',
+    cache: false, 
+    success: function(a) {  
+      if (a) {
+        var html = "";
+        if (season == current) {
+          html += "<p class='page-dialog-heading'>Upcoming Tournaments</p>"
+          html += "<table data-role='table' id='futureTable' class='ui-body-d ui-shadow table-stripe'>"
+               + "<thead><tr class='ui-bar-d'><th>Date</th><th>Game</th><th>Type</th></tr>"
+               + "</thead><tbody>";
+          $.each(a, function(row, data) {
+            var d = new Date(data['date']);
+            if (d > Date.now() ) {
+              html += "<tr><td class='rowpadding'><strong>" + data['date'] + "</strong></td>"
+                   + "<td class='rowpadding'>" + data['game'] + "</td>"
+                   + "<td class='rowpadding'>" + data['type'] + "</td></tr>";
+            }
+          });
+          html += "</tbody></table><br>";
+        }
+        html += "<p class='page-dialog-heading'>Past Tournaments</p>";
+        html += "<table data-role='table' id='futureTable' class='ui-body-d ui-shadow table-stripe'>"
+             + "<thead><tr class='ui-bar-d'><th>Date</th><th>Game</th><th>Type</th></tr>"
+             + "</thead><tbody>";
+        $.each(a, function(row, data) {
+          var d = new Date(data['date']);
+          if (d < Date.now() ) {
+            html += "<tr><td class='rowpadding'><strong>" + data['date'] + "</strong></td>"
+                 + "<td class='rowpadding'>" + data['game'] + "</td>"
+                 + "<td class='rowpadding'>" + data['type'] + "</td></tr>";
+          }
+        });
+        html += "</tbody></table>";
+        $('#scheduleTable').empty();
+        $(html).appendTo('#scheduleTable');
+      }
+    },
+    error: function(err) {
+      alert("Errors in AJAX response from buildScheduleTable in PHP");
+    }
   });
-
 }
--- main.php
+++ main.php
@@ -11,9 +11,23 @@ fnOpenDatabase($DBSERVER,$DBUSER,$DBPASSWD,$DB);
 call_user_func(safe($_POST['todo']));
 
 function buildScheduleTable() {
-  // Build the schedule table
-  $q = "SELECT date,..... and rest of data FROM tournament WHERE season_id = $season";
-  $r = mysql_result($q);
+  if(isset($_POST['season'])) { $season = safe($_POST['season']); }
+  $q = "SELECT tournament.date, games.NAME AS game, event_type.NAME AS type FROM tournament INNER JOIN games "
+     . "ON tournament.game_id = games.GAMEID INNER JOIN event_type ON tournament.event_type = "
+     . "event_type.EVENTID WHERE season_id = $season ORDER BY tournament.date;";
+  $r = mysql_query($q);
+  if ($r) {
+    if (mysql_num_rows($r) > 0) {
+      while ($row = mysql_fetch_array($r)) {
+        $a[] = array('date' => date("M d, Y", strtotime($row['date'])),
+                     'game' => $row['game'],
+                     'type' => $row['type']);
+      }
+      echo json_encode($a);
+    }
+  } else {
+    fnErrorDie("Could not get SQL data in buildScheduleTable");
+  }
   
 }
 
