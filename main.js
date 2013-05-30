1f121aadd032c0561cf49fa2dc9d40f49509790e
Todd Wolfe <wolfet410@yahoo.com>
2013-05-30 16:48:21 -0400

    v0.2.1 - Schedule and rules additions

--- main.js
+++ main.js
@@ -46,14 +46,19 @@ function showSchedule(season) {
         }
         html += "<p class='page-dialog-heading'>Past Tournaments</p>";
         html += "<table data-role='table' id='futureTable' class='ui-body-d ui-shadow table-stripe'>"
-             + "<thead><tr class='ui-bar-d'><th>Date</th><th>Game</th><th>Type</th></tr>"
+             + "<thead><tr class='ui-bar-d'><th>Date</th><th>Game</th><th>Type</th><th>Entrants</th>"
+             + "<th>1st Place</th><th>2nd Place</th><th>3rd Place</th></tr>"
              + "</thead><tbody>";
         $.each(a, function(row, data) {
           var d = new Date(data['date']);
           if (d < Date.now() ) {
             html += "<tr><td class='rowpadding'><strong>" + data['date'] + "</strong></td>"
                  + "<td class='rowpadding'>" + data['game'] + "</td>"
-                 + "<td class='rowpadding'>" + data['type'] + "</td></tr>";
+                 + "<td class='rowpadding'>" + data['type'] + "</td>"
+                 + "<td class='rowpadding'>" + data['entrants'] + "</td>"
+                 + "<td class='rowpadding'>" + data['p1'] + "</td>"
+                 + "<td class='rowpadding'>" + data['p2'] + "</td>"
+                 + "<td class='rowpadding'>" + data['p3'] + "</td></tr>";
           }
         });
         html += "</tbody></table>";
--- main.php
+++ main.php
@@ -12,16 +12,32 @@ call_user_func(safe($_POST['todo']));
 
 function buildScheduleTable() {
   if(isset($_POST['season'])) { $season = safe($_POST['season']); }
-  $q = "SELECT tournament.date, games.NAME AS game, event_type.NAME AS type FROM tournament INNER JOIN games "
+  $q = "SELECT ID, entrants, tournament.date, games.NAME AS game, event_type.NAME AS type FROM tournament INNER JOIN games "
      . "ON tournament.game_id = games.GAMEID INNER JOIN event_type ON tournament.event_type = "
      . "event_type.EVENTID WHERE season_id = $season ORDER BY tournament.date;";
   $r = mysql_query($q);
   if ($r) {
     if (mysql_num_rows($r) > 0) {
       while ($row = mysql_fetch_array($r)) {
+        $q1 = "SELECT CONCAT(player.firstname,' ',player.lastname) FROM tournament_result INNER JOIN player ON "
+            . "tournament_result.player_id = player.ID WHERE position = 1 AND tournament_id =" .$row['ID'];
+        $r1 = mysql_query($q1);
+        $p1 = mysql_result($r1, 0);
+        $q2 = "SELECT CONCAT(player.firstname,' ',player.lastname) FROM tournament_result INNER JOIN player ON "
+            . "tournament_result.player_id = player.ID WHERE position = 2 AND tournament_id =" .$row['ID'];
+        $r2 = mysql_query($q2);
+        $p2 = mysql_result($r2, 0);
+        $q3 = "SELECT CONCAT(player.firstname,' ',player.lastname) FROM tournament_result INNER JOIN player ON "
+            . "tournament_result.player_id = player.ID WHERE position = 3 AND tournament_id =" .$row['ID'];
+        $r3 = mysql_query($q3);
+        $p3 = mysql_result($r3, 0);
         $a[] = array('date' => date("M d, Y", strtotime($row['date'])),
                      'game' => $row['game'],
-                     'type' => $row['type']);
+                     'type' => $row['type'],
+                     'entrants' => $row['entrants'],
+                     'p1' => $p1,
+                     'p2' => $p2,
+                     'p3' => $p3);
       }
       echo json_encode($a);
     }
